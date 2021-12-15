07e0780e648f26c709b526bd7390d10a896de632
frankielam <frankie.lqf@gmail.com>
2021-12-15 22:01:44 +0800

    Golang coding practise

---
+++ example/command-line-arguments.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	p := fmt.Println
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	arg := os.Args[3]
+	p(argsWithProg)
+	p(argsWithoutProg)
+	p(arg)
+}
